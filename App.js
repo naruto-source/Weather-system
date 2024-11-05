c57c7836520236b8addb53d12b00081f0239eb66
Tejas <131481354+naruto-source@users.noreply.github.com>
2024-11-05 19:12:54 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+// import Temp from './Components/Weather/Temp';
+// import Resturant from './Components/Basics/Resturant';
+import Todo from './Components/TodoReact/Todo';
+// import UseState from './Components/Hooks/UseState';
+// import UseEffect from './Components/Hooks/UseEffect';
+// import UseReduer from './Components/Hooks/UseReducer';
+const App = () => {
+  return (
+    <>
+    {/* <Temp></Temp> */}
+    {/* <Resturant></Resturant> */}
+    <Todo></Todo>
+    {/* <UseState></UseState> */}
+    {/* <UseEffect></UseEffect> */}
+    {/* <UseReduer></UseReduer> */}
+  </>
+  )
+}
+
+export default App
